45c283a19043d602866d7158cb132c40c4b7ef11
Nicco Ryan <niccoryan0@gmail.com>
2020-08-10 14:45:52 -0700

    Adding DI and Reposity Design pattern

---
+++ ECommerce-App/ECommerce-App/Models/Interface/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce_App.Models
+{
+    public interface IProduct
+    {
+        /// <summary>
+        /// Read the data from the Cereal csv file
+        /// </summary>
+        /// <returns>List of all data formatted to Products</returns>
+        List<Product> GetProducts();
+    }
+}
