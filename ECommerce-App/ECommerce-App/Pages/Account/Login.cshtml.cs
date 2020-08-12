59b5a6d556637de6696f263dcf375a072b3c3073
Nicco Ryan <niccoryan0@gmail.com>
2020-08-11 18:33:55 -0700

    Got log in page running

--- ECommerce-App/ECommerce-App/Pages/Account/Login.cshtml.cs
+++ ECommerce-App/ECommerce-App/Pages/Account/Login.cshtml.cs
@@ -1,17 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using ECommerce_App.Models;
+using ECommerce_App.Models.Services;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ECommerce_App.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public LoginModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public LoginViewModel Input { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return new LocalRedirectResult("/../..");
+            }
+            return Page();
+        }
+
+        public class LoginViewModel
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
         }
     }
 }
--- ECommerce-App/ECommerce-App/Pages/Account/Register.cshtml.cs
+++ ECommerce-App/ECommerce-App/Pages/Account/Register.cshtml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerce_App.Models;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
