9400654d3e1f1e8eaf3555479ff492edc0606d95
Nicco Ryan <niccoryan0@gmail.com>
2020-08-13 17:35:27 -0700

    Seeded data migration

--- ECommerce-App/ECommerce-App/Migrations/20200814001146_updateSeeds.Designer.cs
+++ ECommerce-App/ECommerce-App/Migrations/20200814001146_updateSeeds.Designer.cs
@@ -3,14 +3,16 @@ using ECommerce_App.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace ECommerce_App.Migrations
+namespace ECommerce_App.Migrations.StoreDb
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200814001146_updateSeeds")]
+    partial class updateSeeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
