d49e3b3c7d454917c8f6316e68c9f18043d0c478
Nicco Ryan <niccoryan0@gmail.com>
2020-08-15 17:00:55 -0700

    Removing Flummeryvms

--- ECommerce-App/ECommerce-App/Models/Interface/IFlummeryInventory.cs
+++ ECommerce-App/ECommerce-App/Models/Interface/IFlummeryInventory.cs
@@ -8,73 +8,73 @@ namespace ECommerce_App.Models.Interface
     public interface IFlummeryInventory
     {
         /// <summary>
-        /// Gets a list of all FlummeryVM objects saved to the database.
+        /// Gets a list of all Flummery objects saved to the database.
         /// </summary>
         /// <returns>
-        /// Task<List<FlummeryVM>>: a List of FlummeryVMs in a Task object
+        /// Task<List<Flummery>>: a List of Flummerys in a Task object
         /// </returns>
         Task<List<Flummery>> GetAllFlummeries();
 
         /// <summary>
-        /// Gets a FlummeryVM by its id.
+        /// Gets a Flummery by its id.
         /// </summary>
         /// <param name="id">
-        /// int: the id of a FlummeryVM
+        /// int: the id of a Flummery
         /// </param>
         /// <returns>
-        /// Task<FlummeryVM>: a FlummeryVM in a Task object if the id exists, null if not
+        /// Task<Flummery>: a Flummery in a Task object if the id exists, null if not
         /// </returns>
         Task<Flummery> GetFlummeryBy(int id);
 
         /// <summary>
-        /// Gets a list of all FlummeryVM objects that match the search term.
+        /// Gets a list of all Flummery objects that match the search term.
         /// </summary>
         /// <param name="term">
         /// string: a search term
         /// </param>
         /// <returns>
-        /// Task<List<FlummeryVM>>: a List of FlummeryVMs in a Task object
+        /// Task<List<Flummery>>: a List of Flummerys in a Task object
         /// </returns>
         Task<List<Flummery>> GetFlummeriesForSearch(string term);
 
         /// <summary>
-        /// Gets a list of all FlummeryVM objects ordered by ordering.
+        /// Gets a list of all Flummery objects ordered by ordering.
         /// </summary>
         /// <param name="ordering">
         /// string: a string describing how to order the list
         /// </param>
         /// <returns>
-        /// Task<List<FlummeryVM>>: a List of FlummeryVMs in a Task object
+        /// Task<List<Flummery>>: a List of Flummerys in a Task object
         /// </returns>
         Task<List<Flummery>> GetFlummeriesOrderedBy(string ordering);
 
         /// <summary>
-        /// Creates a new FlummeryVM object in the database.
+        /// Creates a new Flummery object in the database.
         /// </summary>
         /// <param name="flummeryVM">
-        /// FlummeryVM: the object to be created in the database
+        /// Flummery: the object to be created in the database
         /// </param>
         /// <returns>
-        /// Task<FlummeryVM>: the FlummeryVM object reflecting what was saved to the database, wrapped in a Task
+        /// Task<Flummery>: the Flummery object reflecting what was saved to the database, wrapped in a Task
         /// </returns>
         Task<Flummery> CreateFlummery(Flummery flummery);
 
         /// <summary>
-        /// Updates a FlummeryVM object in the database.
+        /// Updates a Flummery object in the database.
         /// </summary>
         /// <param name="flummeryVM">
-        /// FlummeryVM: the object with updated information
+        /// Flummery: the object with updated information
         /// </param>
         /// <returns>
-        /// Task<FlummeryVM>: the FlummeryVM object reflecting what was saved to the database, wrapped in a Task
+        /// Task<Flummery>: the Flummery object reflecting what was saved to the database, wrapped in a Task
         /// </returns>
         Task<Flummery> UpdateFlummery(Flummery flummery);
 
         /// <summary>
-        /// Deletes a FlummeryVM from the database.
+        /// Deletes a Flummery from the database.
         /// </summary>
         /// <param name="id">
-        /// int: the id of the FlummeryVM to be deleted
+        /// int: the id of the Flummery to be deleted
         /// </param>
         /// <returns>
         /// Task: an empty Task object
